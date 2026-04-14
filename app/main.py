c463dddf00cd9a83006387ed1ef46694da11f87c
Syed Ali Raza Naqvi <alisyednaqvi1512@gmail.com>
2026-04-14 06:44:26 +0500

    main.py updated with trimming router

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.routers import fastqc, health
+from app.routers import fastqc, health, trimming
 
 
 app = FastAPI(
@@ -10,6 +10,7 @@ app = FastAPI(
 
 app.include_router(health.router, prefix="/api/v1")
 app.include_router(fastqc.router, prefix="/api/v1")
+app.include_router(trimming.router, prefix="/api/v1")
 
 @app.get("/")
 async def root():
